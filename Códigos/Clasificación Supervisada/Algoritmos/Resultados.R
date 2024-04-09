be31d2a48e7c918e8ec2566bd15852fca2c1387c
DanielCuaxiloa <78893371+DanielCuaxiloa@users.noreply.github.com>
2024-04-08 19:24:03 -0600

    Update Resultados.R

--- Códigos/Clasificación Supervisada/Algoritmos/Resultados.R
+++ Códigos/Clasificación Supervisada/Algoritmos/Resultados.R
@@ -15,3 +15,12 @@ Modelo_2 <- read.csv(file = "Modelo_2.csv",
 Modelo_3 <- read.csv(file = "Modelo_3.csv", 
                      stringsAsFactors = TRUE)
 
+Datos <- bind_rows(Modelo_1, Modelo_2, Modelo_3) %>% 
+  select(-ID1)
+
+ggplot(data = Datos,
+       mapping = aes(x = Nombre, y = TestGlobal)) +
+  facet_wrap(~Modelo, scales = "free_x") +
+  geom_boxplot(fill = "steelblue3") + 
+  theme_bw()
+
